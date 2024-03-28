1958d1828e4a0ccfe5b7ec7d17850381cb593833
Somya <30110958@adani.com>
2024-03-28 15:18:59 +0530

    Initial Commit

---
+++ Trainman_iOSSDK/Trainman_iOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  Trainman_iOSSDK.h
+//  Trainman_iOSSDK
+//
+//  Created by Somya Badoniya on 28/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Trainman_iOSSDK.
+FOUNDATION_EXPORT double Trainman_iOSSDKVersionNumber;
+
+//! Project version string for Trainman_iOSSDK.
+FOUNDATION_EXPORT const unsigned char Trainman_iOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trainman_iOSSDK/PublicHeader.h>
+
+
